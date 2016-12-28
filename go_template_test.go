ddc99adab6e6dbbfc1fe2c74c0c2975cb0d29ec8
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2016-12-27 20:04:12 -0600

    Add a basic test and an example to GoTemplate function.

--- go_template_test.go
+++ go_template_test.go
@@ -4,9 +4,18 @@ import (
 	"testing"
 )
 
-func BenchmarkExample(t *testing.B) {
+func BenchmarkGoTemplate(t *testing.B) {
 	for i := 0; i < t.N; i++ {
 		// Benchmark test
 		GoTemplate()
 	}
+}
+
+func TestGoTemplate(t *testing.T) {
+	GoTemplate()
+	t.Logf("Test passed")
+}
+
+func ExampleGoTemplate() {
+	GoTemplate()
 }
