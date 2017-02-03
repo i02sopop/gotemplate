27574ed2f9d24cece102e2d88afb9ea645282def
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2017-02-03 00:03:01 -0600

    Rename go_template to gotemplate and add copyright header to all source files.

--- go_template.go
+++
@@ -1,6 +0,0 @@
-// This is a go template
-package go_template
-
-// Go template example function
-func GoTemplate() {
-}
---
+++ gotemplate.go
@@ -0,0 +1,23 @@
+/* Copyright (C) 2016-2017 Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
+
+   This file is part of Gotemplate.
+
+   Gotemplate is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Gotemplate is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Gotemplate. If not, see <http://www.gnu.org/licenses/>.
+*/
+// This is a go template
+package gotemplate
+
+// Go template example function
+func GoTemplate() {
+}
